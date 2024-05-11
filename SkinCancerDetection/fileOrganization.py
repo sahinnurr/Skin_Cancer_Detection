26d309af546435e9c5a6e1770b51ce82447e8549
sudegoktas <goktassu@mef.edu.tr>
2024-05-11 12:01:23 +0300

    explore dataset structure

--- SkinCancerDetection/fileOrganization.py
+++ SkinCancerDetection/fileOrganization.py
@@ -1,6 +1,7 @@
 import os
 import pandas
 import shutil
+import matplotlib.pyplot as plt
 
 #sort images to subfolders
 
@@ -12,6 +13,9 @@ dest_dir = os.getcwd() + "/dataset/reorganized/"
 
 #read the csv file
 skin_df = pandas.read_csv('dataset/HAM10000_metadata.csv')
+
+print(skin_df.head()) #examine the beginning of the dataset
+print(skin_df.info())  #Getting general information of the dataset
 print(skin_df['dx'].value_counts())
 
 label=skin_df['dx'].unique().tolist()  #Extract labels into a list
@@ -25,3 +29,16 @@ for i in label:
     for id in label_images:
         shutil.copyfile((data_dir + "/"+ id +".jpg"), (dest_dir + i + "/"+id+".jpg")) #copying images into corresponding subfolders
     label_images=[]
+
+
+#showing sample images
+fig, axes = plt.subplots(nrows=3, ncols=3, figsize=(10, 10))
+
+for i, ax in enumerate(axes.flat):
+        img_id = skin_df.iloc[i]['image_id']
+        img_path = os.path.join(data_dir, img_id + ".jpg")
+        img = plt.imread(img_path)
+        ax.imshow(img)
+        ax.axis('off')
+
+plt.show()
