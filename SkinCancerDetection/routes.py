ce2b3f5a3a4f690bd9a9ee96368cef1060882898
Nur Şahin <sahinnur@mef.edu.tr>
2024-05-23 03:14:33 +0300

    fixed register backend

--- SkinCancerDetection/routes.py
+++ SkinCancerDetection/routes.py
@@ -1,6 +1,6 @@
+from flask import Blueprint, request, jsonify
 from models import db, User
 from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity
-from flask import Blueprint, request, jsonify
 
 auth_bp = Blueprint('auth', __name__)
 
